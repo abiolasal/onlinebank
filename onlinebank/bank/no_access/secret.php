c0285fa282750bd7aed014ce28ad55f25023e716
abiolasal <64172737+abiolasal@users.noreply.github.com>
2020-05-15 11:26:58 -0500

    Update secret.php

--- onlinebank/bank/no_access/secret.php
+++ onlinebank/bank/no_access/secret.php
@@ -4,11 +4,11 @@
     // the file perms are 751 -> notice not world readable. 
 
     // this is the connection string for the server. no need to change this, it should just work. 
-    $db_connection_string = 'mysql:dbname=test;host=student-db.cse.unt.edu';
+    $db_connection_string = 'mysql:dbname=test;host=localhost';
 
     // these variables are required to connect to the database mentioned above.
     // please fill these out with the correct information. Don't forget to change your passwords.
-    $db_user='cac0554';
-    $db_password='thisisabankingprojectforschool';
+    $db_user='';
+    $db_password='';
 ?>
 
